fffe0cc26e7f920a0a8bd4551670224e9a27e572
Elvis Iradukunda <student@Students-MacBook-Pro.local>
2024-10-08 16:38:03 +1300

    Elvis'sCounter-project

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// This function adds one to the counter. When you click on "Add one more", the counter text changes to reflect how many times the button has been clicked.
+
 let count = 0;
 document.getElementById('add').addEventListener('click', function() {
     count++;
     document.getElementById('counts').innerText = count;
 });
+
+// This function changes the grammar text to plural after its first click been clicked.
 
 document.getElementById('add').addEventListener('click', function() {
     if (count === 1) {
