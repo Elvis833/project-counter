08c718edee3d60e8fa5df079c4904f521e9ef0c6
Elvis Iradukunda <student@Students-MacBook-Pro.local>
2024-10-08 14:47:04 +1300

    Elvis'sCounter-project

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('add').addEventListener('click', function() {
     count++;
     document.getElementById('counts').innerText = count;
 });
+
+document.getElementById('add').addEventListener('click', function() {
+    if (count === 1) {
+        document.getElementById('counter').innerText = 'Clicked ' + count + ' time';
+    } else {
+        document.getElementById('counter').innerText = 'Clicked ' + count + ' times';
+    }
+});
