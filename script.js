b393b043cc0417e06cc10d2a1540aaa0d9da26b5
Student2 <student2@Students-MacBook-Pro.local>
2024-10-23 14:47:17 +1300

    Elvis'sCounter-App

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// This function adds one to the counter. When you click on "Add one more", the counter text changes to reflect how many times the button has been clicked.
-
+// This function adds one to the counter When you click on "Add", the counter text changes to reflect how many times the button has been clicked.
 let count = 0;
 document.getElementById('add').addEventListener('click', function() {
     count++;
-    document.getElementById('counts').innerText = count;
+    updateDisplay();
 });
-
+// This function subtracts one to the counter When you click on "subtract" the counter text changes to reflect how many times the button has been clicked.
+document.getElementById('subtract').addEventListener('click', function() {
+        count--;  
+        updateDisplay();
+});
 // This function changes the grammar text to plural after its first click been clicked.
-
-document.getElementById('add').addEventListener('click', function() {
+function updateDisplay() {
     if (count === 1) {
         document.getElementById('counter').innerText = 'Clicked ' + count + ' time';
     } else {
         document.getElementById('counter').innerText = 'Clicked ' + count + ' times';
     }
-});
+};
